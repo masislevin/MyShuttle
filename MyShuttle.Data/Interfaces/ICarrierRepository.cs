cc52dd79c8f0daf34e19924c018728ab02ba6a8a
Levin Masis <masislevin@gmail.com>
2020-04-07 11:47:33 +0300

    Add project files.

---
+++ MyShuttle.Data/Interfaces/ICarrierRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyShuttle.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShuttle.Data.Interfaces
+{
+    public interface ICarrierRepository
+    {
+        Task<int> AddAsync(Carrier carrier);
+        Task<SummaryAnalyticInfo> GetAnalyticSummaryInfoAsync(int carrierId);
+        Task<Carrier> GetAsync(int carrierId);
+        Task<List<Carrier>> GetCarriersAsync(string filter);
+        Task UpdateAsync(Carrier carrier);
+
+    }
+}
